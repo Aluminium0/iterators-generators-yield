5bc467831a89472e63051cfa87f8123cc3c96a87
Rene Grempel <grempelrene@gmail.com>
2026-04-16 11:33:39 +0200

    Update task_1_flat_iterator.py

--- task_1_flat_iterator.py
+++ task_1_flat_iterator.py
@@ -1,4 +1,5 @@
 class FlatIterator:
+
     def __init__(self, list_of_list):
         self.list_of_list = list_of_list
 
@@ -16,3 +17,25 @@ class FlatIterator:
             self.outer_index += 1
             self.inner_index = 0
         raise StopIteration
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
